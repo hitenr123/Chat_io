2ba5d5e6eaf4875da426bda9cdd11602f1176aa0
Hiten Ranjan <pranjalkeshari2433@gmail.com>
2026-03-18 16:10:23 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,16 @@
 
+var socket = io();
+
+function sendMessage() {
+    let input = document.getElementById("message");
+    socket.send(input.value);
+    input.value = "";
+}
+
+socket.on("message", function(msg) {
+    let div = document.createElement("div");
+    div.classList.add("msg");
+    div.innerText = msg;
+
+    document.getElementById("messages").appendChild(div);
+});
