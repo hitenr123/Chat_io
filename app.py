9d1f3e311d3660974ec834ba3ef85bf06c1d4969
HitenR123 <pranjalkeshari2433@gmail.com>
2026-03-19 22:34:03 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import os
 import mysql.connector
 
 app = Flask(__name__)
+CORS(app)   # enable CORS
 
 
 def get_db():
