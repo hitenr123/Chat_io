6862207e9b3c276dc1d5ee2dfdaef574e806d209
HitenR123 <pranjalkeshari2433@gmail.com>
2026-03-19 21:31:43 +0530

    Refactor app.py to initialize Flask app and correct template path

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+from flask import Flask, render_template
 import os
 
-port = int(os.environ.get("PORT", 5000))
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template("../templates/index.html")
+    return render_template("index.html")
 
-app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
