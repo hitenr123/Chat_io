e9f10e3579dc79fe41ea391acebdab78b8efa27b
HitenR123 <pranjalkeshari2433@gmail.com>
2026-03-21 14:50:55 +0530

    adding new pop up system

--- static/script.js
+++ static/script.js
@@ -6,54 +6,90 @@ function toggleForm() {
   register.classList.toggle("active");
 }
 
+function showPopup(message, type) {
+  const overlay = document.getElementById("popupOverlay");
+  const popup = overlay.querySelector(".popup");
+  const text = document.getElementById("popupText");
+
+  text.innerText = message;
+
+  popup.classList.remove("success", "error");
+
+  if (type === "success") {
+    popup.classList.add("success");
+  } else {
+    popup.classList.add("error");
+  }
+
+  overlay.style.display = "flex";
+
+  setTimeout(() => {
+    overlay.style.display = "none";
+  }, 2500);
+}
 
 // REGISTER
-document.getElementById("registerForm").addEventListener("submit", async function(e){
-  e.preventDefault();
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
 
-  const username = this.querySelector("input[name='username']").value;
-  const email = this.querySelector("input[name='email']").value;
-  const password = this.querySelector("input[name='password']").value;
+    const username = this.querySelector("input[name='username']").value;
+    const email = this.querySelector("input[name='email']").value;
+    const password = this.querySelector("input[name='password']").value;
 
-  const res = await fetch("https://web-production-3a7ea.up.railway.app/register", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    username: username,
-    email: email,
-    password: password
-  })
-})
-.then(res => res.json())
-.then(data => console.log(data));
+    const res = await fetch(
+      "https://web-production-3a7ea.up.railway.app/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      },
+    );
 
-  const data = await res.json();
-  console.log(data);
-});
-
+    const data = await res.json();
+    console.log(data);
+    if (data.status === "success") {
+      showPopup("Registration Successful 🎉", "success");
+    } else {
+      showPopup("Registration Failed ❌", "error");
+    }
+  });
 
 // LOGIN
-document.getElementById("loginForm").addEventListener("submit", async function(e){
-  e.preventDefault();
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
 
-  const username = this.querySelector("input[name='username']").value;
-  const password = this.querySelector("input[name='password']").value;
+    const username = this.querySelector("input[name='username']").value;
+    const password = this.querySelector("input[name='password']").value;
 
-  fetch("https://web-production-3a7ea.up.railway.app/register", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    username: username,
-    password: password
-  })
-})
-.then(res => res.json())
-.then(data => console.log(data));
+    const res = await fetch(
+      "https://web-production-3a7ea.up.railway.app/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      },
+    );
 
-  const data = await res.json();
-  console.log(data);
-});
+    const data = await res.json();
+    console.log(data);
+    if (data.status === "success") {
+      showPopup("Login Successful 🎉", "success");
+    } else {
+      showPopup("Invalid Username or Password ❌", "error");
+    }
+  });
